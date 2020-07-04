01d8027629f6b3d8d65cbed279bd72a445cdd38e
rishh <rihwariranjan@gmail.com>
2020-07-05 00:33:08 +0530

    added random function

--- app.js
+++ app.js
@@ -76,9 +76,14 @@ document.addEventListener('DOMContentLoaded' ,()=> {
         // }
     ]
 
+    cardarrya.sort(() => 0.5 - Math.random())
+
 
     const grid = document.querySelector('.grid')
-
+    const resultDisplay = Document.querySelectorAll('#result')
+    var cardschosen = []
+    var cardschosenId =[]
+    var cardswon = []
     //creating board
     function createBoard(){
         for (let i =0;i<cardarrya.length;i++)
@@ -87,11 +92,63 @@ document.addEventListener('DOMContentLoaded' ,()=> {
             card.setAttribute('src', 'images/blank.png')
             card.setAttribute('data-id',i)
            // card.addEventListener('click',flipcard)
-            grid.appendChild(card)
+            grid.appendChild(cardarrya[cardId.name])
+            cardschosenId.push(cardId)
         }
     }
 
-    createBoard()
+    //check for match
+    function checkforMatch()
+    {
+        var cards = document.querySelectorAll('img')
+        const optiononeid = cardschosenId[0]
+        const optiontwoid = cardschosenId[1]
+
+        if(cardschosenId[0]==cardschosenId[1])
+        {
+            alert('You found a match')
+            cards[optiononeid].setAttribute('src','images/white.png')
+            cards[optiontwoid].setAttribute('src','images/white.png')
+            cardswon.push(cardschosen)
+
+
+        }
+
+        else
+        {
+            cards[optiononeid].setAttribute('src','images/blank.png')
+            cards[optiontwoid].setAttribute('src','images/blank.png')
+            alert('Sorry Try Again!')
+
+        }
+
+        cardschosen = []
+        cardschosenId =[]
+        resultDisplay.textContent = cardswon.length
+
+        if(cardswon.length== cardarrya.length/2)
+
+        {
+            resultDisplay.textContent = 'Congratulations! You Won !!'
+            
+        }
+    }
+    //createBoard()
+
+    //flipcard 
+
+    function flipCard()
+    {
+        var cardId = this.getAttribute('data-id')
+        cardschosen.push(cardarrya[cardId].name)
+        cardschosenId.push(cardId)
+        this.setAttribute('src', cardarrya[cardId].img)
+        if(cardschosen.length == 2)
+        {
+            setTimeout(checkforMatch,500)
+        }
+
+    }
 
 }
 
